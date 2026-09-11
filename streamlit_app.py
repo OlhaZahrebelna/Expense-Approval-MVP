6c74c7b699d1a61ff6bf5e13fc1a6b44e6105869
ZahrebelnaOlha <zahrebelnaolha2@gmail.com>
2026-09-11 19:29:16 +0200

    added simple interface

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Expense Approval MVP",
+    page_icon="💰",
+    layout="wide",
+)
+
+
+st.title("Expense Approval MVP")
+
+st.write("Streamlit interface is running.")
